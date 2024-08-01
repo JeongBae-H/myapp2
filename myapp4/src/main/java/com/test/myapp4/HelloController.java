2d2e817b8238067321afb2061d909eecd9b7b521
Hwang JeongBae <hjb0403@naver.com>
2024-08-01 10:12:30 +0900

    ok

---
+++ myapp4/src/main/java/com/test/myapp4/HelloController.java
@@ -0,0 +1,15 @@
+package com.test.myapp4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "Hong Gildong");
+        return "hello";
+    }
+}
