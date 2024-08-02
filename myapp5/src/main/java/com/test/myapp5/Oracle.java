4148d7946f81642fe3fddb40286566e60bff4664
Hwang JeongBae <hjb0403@naver.com>
2024-08-02 11:22:50 +0900

    ok

--- myapp5/src/main/java/com/test/myapp5/Oracle.java
+++ myapp5/src/main/java/com/test/myapp5/Oracle.java
@@ -1,8 +1,11 @@
 package com.test.myapp5;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+// 캡슐화, 다형성, 상속성, 형번환(값타입 형변환, 참조타입 형변환)
 @Component
+@Primary
 public class Oracle implements Database{
     @Override
     public void open() {
